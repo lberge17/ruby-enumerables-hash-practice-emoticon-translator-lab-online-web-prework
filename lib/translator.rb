79ac6feea26b126ac4787a746e3874c47c16b854
Laura Berge <lauraeberge@gmail.com>
2019-08-30 20:11:10 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,7 +7,7 @@ def load_library(file_path)
   library.each do |library_function, library_hash|
     emoticons.each do |emoticons_meaning, emoticons_array|
       if library_function == "get_meaning"
-        library_hash << {"#{emoticons_array[1]}" => emoticons_meaning}
+        library_hash  {"#{emoticons_array[1]}" => emoticons_meaning}
         puts library_hash
       elsif library_function == "get_emoticon"
         library_hash << {"#{emoticons_array[0]}" => emoticons_meaning}
