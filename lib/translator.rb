60cad7d5ed03985e8dd1f29c04faa06a870e06d1
Laura Berge <lauraeberge@gmail.com>
2019-08-30 20:29:55 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,8 +18,7 @@ end
 
 def get_japanese_emoticon(file_path, english_emoticon)
   load_library(file_path)
-  japan_emoji = library["get_emoticon"][english_emoticon]
-  japan_emoji
+  puts library["get_emoticon"][english_emoticon]
 end
 
 def get_english_meaning
