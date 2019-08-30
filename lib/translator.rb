af429c565394ded82dc76fa4cfc72ecf254c7009
Laura Berge <lauraeberge@gmail.com>
2019-08-30 19:29:31 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,18 @@
-# require modules here
+require "yaml"
 
-def load_library
-  # code goes here
+def load_library(file_path)
+  library = {:get_meaning => {}, :get_emoticon => {}}
+  emoticons = YAML.load_file(file_path)
+  
+  library.each do |library_key, library_hash|
+    emoticons.each do |emoticons_key, emoticons_array|
+      
+    end
+  end
+  library
 end
+
+load_library("./lib/emoticons.yml")
 
 def get_japanese_emoticon
   # code goes here
