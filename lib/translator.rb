c223f0cc0baf75c6afd37ea3add02956a99e101b
Laura Berge <lauraeberge@gmail.com>
2019-08-30 20:42:14 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -26,6 +26,8 @@ def get_japanese_emoticon(file_path, english_emoticon)
   end
 end
 
-def get_english_meaning(file_path, emoticon)
+def get_english_meaning(file_path, japanese_emoticon)
   library = load_library(file_path)
+  meaning = library["get_meaning"][japanese_emoticon]
+  meaning
 end
